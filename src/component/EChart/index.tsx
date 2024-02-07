1d31a2b693624318ec44b59605016f854200ff06
WJQ <1092229612@qq.com>
2024-02-07 20:19:58 +0800

    feat: 封装echarts

---
+++ src/component/EChart/index.tsx
@@ -0,0 +1,84 @@
+import * as echarts from 'echarts';
+import { EChartsType } from 'echarts';
+import { debounce } from 'lodash-es';
+import React, {
+  useEffect,
+  useImperativeHandle,
+  useLayoutEffect,
+  useRef,
+} from 'react';
+
+interface EChartProps {
+  options: echarts.EChartsOption;
+  style?: React.CSSProperties;
+  onClick?: (params: any) => void;
+  loading?: boolean;
+}
+
+const EChart = (props: EChartProps, ref: any) => {
+  const { options, style = {}, onClick, loading = false } = props;
+  const cDom = useRef(null);
+  const cInstance = useRef<EChartsType | undefined>();
+
+  const getInstance = () => cInstance.current;
+  useImperativeHandle(ref, () => ({ getInstance }));
+
+  useEffect(() => {
+    if (cDom.current) {
+      // 校验 Dom 节点上是否已经挂载了 ECharts 实例，只有未挂载时才初始化
+      cInstance.current = echarts.getInstanceByDom(cDom.current);
+      if (!cInstance.current) {
+        cInstance.current = echarts.init(cDom.current, null, {
+          renderer: 'svg',
+        });
+      }
+      // 设置配置项
+      options && cInstance.current.setOption(options);
+      // 绑定点击事件
+      cInstance.current.on('click', (event) => {
+        if (event && onClick) onClick(event);
+      });
+    }
+
+    return () => {
+      // 容器被销毁之后，销毁实例，避免内存泄漏
+      cInstance.current?.dispose();
+    };
+  }, [cDom, options]);
+
+  // 展示 loading 动画
+  useEffect(() => {
+    loading
+      ? cInstance.current?.showLoading()
+      : cInstance.current?.hideLoading();
+  }, [loading]);
+
+  // 窗口自适应并开启过渡动画
+  const resize = () => {
+    if (cInstance.current) {
+      cInstance.current.resize({
+        animation: { duration: 300 },
+      });
+    }
+  };
+
+  const debounceResize = debounce(resize, 500);
+
+  // 监听窗口大小
+  useEffect(() => {
+    window.addEventListener('resize', debounceResize);
+
+    return () => {
+      window.removeEventListener('resize', debounceResize);
+    };
+  }, []);
+
+  // 监听高度变化
+  useLayoutEffect(() => {
+    debounceResize();
+  }, [style.width, style.height]);
+
+  return <div ref={cDom} style={{ width: '100%', height: '80%', ...style }} />;
+};
+
+export default React.memo(React.forwardRef(EChart));
--- src/pages/Monitor/index.tsx
+++ src/pages/Monitor/index.tsx
@@ -1,9 +1,48 @@
+import EChart from '@/component/EChart';
 import { PageContainer } from '@ant-design/pro-components';
+import styles from './index.module.less';
 
 const Monitor: React.FC = () => {
   return (
-    <PageContainer ghost>
-      <div>Monitor</div>
+    <PageContainer ghost title={false}>
+      <div className={styles['monitors']}>
+        <EChart
+          style={{ width: '50%' }}
+          options={{
+            xAxis: {
+              type: 'category',
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            },
+            yAxis: {
+              type: 'value',
+            },
+            series: [
+              {
+                data: [150, 230, 224, 218, 135, 147, 260],
+                type: 'line',
+              },
+            ],
+          }}
+        />
+        <EChart
+          style={{ width: '50%' }}
+          options={{
+            xAxis: {
+              type: 'category',
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            },
+            yAxis: {
+              type: 'value',
+            },
+            series: [
+              {
+                data: [150, 230, 224, 218, 135, 147, 260],
+                type: 'line',
+              },
+            ],
+          }}
+        />
+      </div>
     </PageContainer>
   );
 };
